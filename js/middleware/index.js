6cdb4b46b4ffa7d42b2aa1b54c7896b549dfe332
Daniel Miller <daniel.miller5383@gmail.com>
2018-03-13 16:13:24 -0400

    started refactoring so that youtube can be called on scroll

--- js/middleware/index.js
+++ js/middleware/index.js
@@ -5,27 +5,33 @@ import {receiveVideos} from '../actions';
 
 const ytMiddleware = store => next => action => {
     if (action.type == FETCH_VIDEOS) {
-        let youTube = new YouTube(); 
-        youTube.setKey('AIzaSyBhiCYZwT2PW7kZ_LUDGv4cyFm4K7zegDI'); 
-        youTube.search(action.payload, 10, {pageToken: ''}, function(error, result){
-            if(error) {
-                return 'error';
-            } else {
-                let videos = result.items;
-                let videoList = [];
-                for(let i=0; i<videos.length; i++) {
-                    let video = {};
-                    
-                    video.thumbnail = videos[i].snippet.thumbnails.medium.url;
-                    video.title = videos[i].snippet.title;
-                    videoList.push(video);
-                }
-                store.dispatch(receiveVideos(videoList));
-             
-            }
-        }) 
+        let category = action.payload
+        getYTVideos(action, store);    
     }
     return next(action);
+}
+
+function getYTVideos(action, store) {
+    let youTube = new YouTube(); 
+    let pageToken = store.getState().videos.pageToken;
+    let videoList = store.getState().videos.videoList;
+    let category = action.payload;
+    youTube.setKey('AIzaSyBhiCYZwT2PW7kZ_LUDGv4cyFm4K7zegDI'); 
+    youTube.search(category, 10, {pageToken: pageToken}, function(error, result){
+        if(error) {
+            return 'error';
+        } else {
+            let videos = result.items;
+            for(let i=0; i<videos.length; i++) {
+                let video = {};
+                video.thumbnail = videos[i].snippet.thumbnails.medium.url;
+                video.title = videos[i].snippet.title;
+                videoList.push(video);
+            }
+            store.dispatch(receiveVideos(videoList));
+        }
+    });
+
 }
 
 const crashReporter = store => next => action => {
