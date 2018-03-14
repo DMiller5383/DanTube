1c7a7e2c964916dbf922639d7099e9f6a0ff5c1e
Daniel Miller <daniel.miller5383@gmail.com>
2018-03-13 21:18:54 -0400

    changes isFetching for scroll

--- js/reducers/reducer_videos.js
+++ js/reducers/reducer_videos.js
@@ -1,8 +1,12 @@
-import { FETCH_VIDEOS, RECEIVE_VIDEOS } from '../actions';
+import { FETCH_VIDEOS, RECEIVE_VIDEOS, UPDATE_FETCH_VIDEOS } from '../actions';
 
 export default function(state={}, action) {
     switch(action.type) {
         case FETCH_VIDEOS:
+            return Object.assign({}, state, {
+                isFetching: true
+            });
+        case UPDATE_FETCH_VIDEOS:
             return Object.assign({}, state, {
                 isFetching: true
             });
